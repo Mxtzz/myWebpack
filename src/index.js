a5caa2a432c6453784167e22be0840a9776fbc85
Even M <mazw2016@hotmail.com>
2020-02-03 21:51:39 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import img1 from './img1.jpg';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var myImg = new Image();
+    myImg.src = img1;
+    myImg.height = 200;
+    element.appendChild(myImg);
 
     return element;
 }
