0eb31df1be1dd75e13942deb6ebee89e0823f75b
Even M <mazw2016@hotmail.com>
2020-02-06 22:07:55 +0800

    Output Management init

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import img1 from './img1.jpg';
-import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    var myImg = new Image();
-    myImg.src = img1;
-    myImg.height = 200;
-    element.appendChild(myImg);
-
-    console.log(Data);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,5 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
+
 };
